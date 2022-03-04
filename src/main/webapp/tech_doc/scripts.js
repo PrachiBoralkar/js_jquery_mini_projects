b9fd47a0ef58fe18e00d8ca20f2bc40848078ce0
Prachi Boralkar <boralkarprachi@gmail.com>
2022-03-05 02:36:05 +0530

    p

---
+++ src/main/webapp/tech_doc/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+	$('#l1').click(function () {
+		
+		$('#target').load('intro.html');
+		
+	});
+});
+
+$(document).ready(function () {
+	$('#l2').click(function() {
+		$('#target').load('why.html');
+	});
+});
+
+$(document).ready(function () {
+	$('#l3').click(function() {
+		$('#target').load('js_java.html');
+	});
+});
+
+$(document).ready(function () {
+	$('#l4').click(function() {
+		$('#target').load('hw.html');
+	});
+});
+
+$(document).ready(function () {
+	$('#l5').click(function() {
+		$('#target').load('var.html');
+	});
+});
+
+$(document).ready(function () {
+	$('#l6').click(function() {
+		$('#target').load('vard.html');
+	});
+});
+
+$(document).ready(function () {
+	$('#l7').click(function() {
+		$('#target').load('varscope.html');
+	});
+});
+
+  
+
+
+
+
